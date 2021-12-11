a011c5b0cbad9eb0de58373d23581c0ec12c15b4
Jose Velarde <josevelarde00@gmail.com>
2021-12-11 04:32:40 -0300

    Added django-heroku

--- core/settings.py
+++ core/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
+import django_heroku
 
 import os
 from pathlib import Path
@@ -176,3 +177,4 @@ DATABASES["default"].update(db_from_env)
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+django_heroku.settings(locals())
