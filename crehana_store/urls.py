2677503d064005c1a65e496a3bc7c06967be53c2
Jose Velarde <josevelarde00@gmail.com>
2021-12-30 19:39:05 -0300

    Clean up code

--- crehana_store/urls.py
+++ crehana_store/urls.py
@@ -1,15 +1,15 @@
-from django.urls import path, include
+from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 from . import views
 
 app_name = "crehana_store"
 
 router = DefaultRouter()
-router.register(r'courses', views.CourseViewSet)
-router.register(r'categories', views.CategoryViewSet)
-router.register(r'subcategories', views.SubcategoryViewSet)
+router.register(r"courses", views.CourseViewSet)
+router.register(r"categories", views.CategoryViewSet)
+router.register(r"subcategories", views.SubcategoryViewSet)
 
 # The API URLs are now determined automatically by the router.
 urlpatterns = [
-    path('', include(router.urls)),
+    path("", include(router.urls)),
 ]
--- crehana_store/views.py
+++ crehana_store/views.py
@@ -1,26 +1,30 @@
-from rest_framework import viewsets
-
-
-from .models import Category, Course, Subcategory
-from .serializers import CourseSerializer, CategorySerializer, SubcategorySerializer
-from .filters import CourseFilter
 from core.pagination import NoPagination
+from rest_framework import authentication, permissions, viewsets
+from django.views.decorators.csrf import ensure_csrf_cookie
+
+from .filters import CourseFilter
+from .models import Category, Course, Subcategory
+from .serializers import (
+    CategorySerializer,
+    CourseSerializer,
+    SubcategorySerializer,
+
+)
 
 # Create your views here.
 
-
 class SubcategoryViewSet(viewsets.ReadOnlyModelViewSet):
     pagination_class = NoPagination
-    queryset = Subcategory.objects.all().order_by('id')
+    queryset = Subcategory.objects.all().order_by("id")
     serializer_class = SubcategorySerializer
 
 class CategoryViewSet(viewsets.ReadOnlyModelViewSet):
     pagination_class = NoPagination
-    queryset = Category.objects.all().order_by('id')
+    queryset = Category.objects.all().order_by("id")
     serializer_class = CategorySerializer
 
 class CourseViewSet(viewsets.ReadOnlyModelViewSet):
 
     filter_class = CourseFilter
-    queryset = Course.objects.all().order_by('id')
+    queryset = Course.objects.all().order_by("id")
     serializer_class = CourseSerializer
